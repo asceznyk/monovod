cbacf3a3a4322b92d8f5a15a5134af8f3e3dfc56
asceznyk <asceznyk@gmail.com>
2022-08-04 10:54:18 +0530

    orb features testing..

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 orb = cv2.ORB_create()
 
 def orb_keyframe(img):
-    gx, gy = 4, 4
+    gx, gy = 10, 10
     w,h = img.shape[0] // 2, img.shape[1] // 2
     img = cv2.resize(img, (h,w)) 
 
