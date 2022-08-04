032af42cffb0619025f7f2ac11e396bef958fdec
asceznyk <asceznyk@gmail.com>
2022-08-04 11:16:38 +0530

    orb features testing..

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 orb = cv2.ORB_create(nfeatures=100000)
 
 def orb_keyframe(img):
-    w,h = img.shape[0] // 2, img.shape[1] // 2 
     kps, des = orb.compute(img, orb.detect(img))
     return cv2.drawKeypoints(img, kps, None, color=(0,255,0), flags=0)
 
@@ -26,6 +25,7 @@ def vslam(video_path, orb, max_len=1):
     while ret and f < max_len: 
         ret, img = cap.read()
         if ret:
+            w,h = img.shape[0] // 2, img.shape[1] // 2 
             img = cv2.resize(img, (h,w)) 
             #kpimg = orb_keyframe(img)
             kpimg = gftt_keyframe(img)
