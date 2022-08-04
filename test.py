1904c277c1357d289ec35bb50c56b02b72ba3696
asceznyk <asceznyk@gmail.com>
2022-08-04 11:14:13 +0530

    orb features testing..

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def orb_keyframe(img):
 
 def gftt_keyframe(img):
     pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=7)
-    kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in pts]
+    kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in pts]
     kps, des = orb.compute(img, kps)
     return cv2.drawKeypoints(img, kps, None, color=(0,255,0), flags=0)
 
