23bea19f94fb1ce1f94b120cf1a107198de8a134
asceznyk <asceznyk@gmail.com>
2022-08-04 11:24:19 +0530

    orb features testing..

--- test.py
+++ test.py
@@ -7,10 +7,6 @@ import matplotlib.pyplot as plt
 
 orb = cv2.ORB_create()
 
-def orb_keyframe(img):
-    kps, des = orb.compute(img, orb.detect(img))
-    return cv2.drawKeypoints(img, kps, None, color=(0,255,0), flags=0)
-
 def gftt_keyframe(img):
     pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=7)
     kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in pts]
@@ -27,7 +23,6 @@ def vslam(video_path, orb, max_len=1):
         if ret:
             w,h = img.shape[0] // 2, img.shape[1] // 2 
             img = cv2.resize(img, (h,w)) 
-            #kpimg = orb_keyframe(img)
             kpimg = gftt_keyframe(img)
             plt.imshow(kpimg)
             plt.show()
