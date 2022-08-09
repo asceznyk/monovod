d5762148a9691f8c3a4ca27921c8a1b0e4952bcc
asceznyk <asceznyk@gmail.com>
2022-08-09 18:11:40 +0530

    horrible performance! yay!

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import cv2
 
@@ -5,23 +6,59 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+from plotting import *
 from vslam import *
 
-def run(video_path):
+def init_image_scale(img, scl=2):
+    h, w = img.shape[1]//scl, img.shape[0]//scl
+    fx, fy = 716//scl, 716//scl ##these are pre-selected values 
+    cm = np.array([
+        [fx, 0, w//2],
+        [0, fy, h//2],
+        [0, 0, 1],
+    ])
+
+    return h, w, cm, (fx, fy)
+
+def read_gt(file_path):
+    poses = []
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            t = np.fromstring(line, dtype=np.float64, sep=' ')
+            t = t.reshape(3, 4)
+            t = np.vstack((t, [0, 0, 0, 1]))
+            poses.append(t)
+    return poses
+
+def run(video_path, txt_path):
     cap = cv2.VideoCapture(video_path)
     ret = True
     f = 0
 
-    vslam = vSLAM()
+    _, img = cap.read()
+    h, w, cm, _ = init_image_scale(img, scl=1)
+    vslam = vSLAM(cm)
+    cur_pose = np.eye(4)
+
+    est_path = []
+    gt_path = [] 
+    print(gt_path)
+    gt_poses = read_gt(txt_path)
     while ret: 
         ret, img = cap.read()
         if ret:
-            w,h = img.shape[0] // 2, img.shape[1] // 2 
             img = cv2.resize(img, (h,w)) 
             if f > 1: 
-                q1, q2 = vslam.get_matches() 
+                q1, q2 = vslam.get_matches()
+                tsfm = vslam.get_pose(q1,q2)
+                cur_pose = np.matmul(cur_pose, np.linalg.inv(tsfm))
             f = vslam.add_frame(img)
+
+        gt_path.append((gt_poses[f][0, 3], gt_poses[f][2, 3]))
+        est_path.append((cur_pose[0, 3], cur_pose[2, 3]))
+    
+    visualize_paths(gt_path, est_path, f"vSLAM_{video_path}", file_out=os.path.basename(video_path.replace('.avi', '')) + ".html")
     return
 
-run(sys.argv[1])
+run(sys.argv[1], sys.argv[2])
 
--- vslam.py
+++ vslam.py
@@ -2,12 +2,15 @@ import cv2
 import numpy as np
 
 class vSLAM():
-    def __init__(self):
+    def __init__(self, cm):
         self.orb = cv2.ORB_create(3000) 
         index_params = dict(algorithm=6, table_number=6, key_size=12, multi_probe_level=1)
         search_params = dict(checks=50)
         self.flann = cv2.FlannBasedMatcher(indexParams=index_params, searchParams=search_params)
         self.frames = []
+        self.cm = cm
+
+        print(self.cm)
 
     def add_frame(self, f):
         self.frames.append(f)
@@ -31,9 +34,21 @@ class vSLAM():
                                flags = 2)
 
             img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
-            cv2.imshow('fuck',img3)
+            cv2.imshow('fuck_you',img3)
             cv2.waitKey(200)
 
         return np.float32([kp1[m.queryIdx].pt for m in good]), np.float32([kp2[m.trainIdx].pt for m in good])
+
+    def tsfm_mat(self, r, t):
+        z = np.eye(4, dtype=np.float64)
+        z[:3, :3] = r
+        z[:3, 3] = t
+        return z
+
+    def get_pose(self, q1, q2):
+        e, _ = cv2.findEssentialMat(q1, q2, self.cm, threshold=1)
+        r, _, t = cv2.decomposeEssentialMat(e) 
+        return self.tsfm_mat(r, t.flatten())
+
 
 
