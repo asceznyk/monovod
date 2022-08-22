7251c148c88c83d857a62528f0da4bb25093fb07
asceznyk <asceznyk@gmail.com>
2022-08-22 12:09:27 +0530

    folder for outputs

--- pangolin_install.md
+++ pangolin_install.md
@@ -3,7 +3,7 @@ git clone https://github.com/uoip/pangolin.git
 cd pangolin
 mkdir build
 cd build
-cmake -DBUILD_PANGOLIN_FFMPEG=OFF -DPYBIND11_PYTHON_VERSION=3.8 ..
+cmake -DBUILD_PANGOLIN_FFMPEG=OFF -DPYBIND11_PYTHON_VERSION=3.10 ..
 make -j8
 cd ..
 python3 setup.py install
--- run.py
+++ run.py
@@ -13,7 +13,7 @@ from monovod import *
 from plotting import *
 from display import *
 
-def main(video_path, poses_path=None, calibs_path=None, focal_length=716):
+def main(video_path, poses_path=None, calibs_path=None, output_dir='html', focal_length=716):
     cap = cv2.VideoCapture(video_path)
     ret = True 
 
@@ -52,13 +52,14 @@ def main(video_path, poses_path=None, calibs_path=None, focal_length=716):
         if poses_path is not None: gt_path.append((gt_poses[f][0, 3], gt_poses[f][2, 3])) 
         est_path.append((cur_pose[0, 3], cur_pose[2, 3])) 
     video_name = os.path.basename(video_path).split('.')[0]
-    visualize_paths(est_path, gt_path, f"MONOVOD_{video_name}", file_out= f"{video_name}.html") 
+    visualize_paths(est_path, gt_path, f"MONOVOD_{video_name}", file_out= f"{output_dir}/{video_name}.html") 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--video_path', type=str, required=True, help='path to the video you want to track')
     parser.add_argument('--poses_path', type=str, help='text file contatining the pose matrix for each frame in video')
     parser.add_argument('--calibs_path', type=str, help='text file contatining the projection matrix')
+    parser.add_argument('--output_dir', type=str, default='html', help='folder to save html output files')
     parser.add_argument('--focal_length', type=int, default=716, help='focal length of the camera for building projection matrix if calibration path is not given')
 
     args = parser.parse_args()
